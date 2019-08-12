ba375ca3aea7afdfb12b488b3cc655ec4141241e
Noor Al Darraji <077068@yahoo.com>
2019-08-12 16:31:55 -0500

    Step 2 done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context API
+import { ProductContext } from './context/ProductContext'
+
+export const ProductContext = createContext();
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -17,24 +21,19 @@ function App() {
 
 	return (
 		<div className="App">
+		<ProductContext.Provider value={{ products, addItem }}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
+			<Route exact path="/" component={Products} />
 				)}
 			/>
 
 			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				path="/cart" component={ShoppingCart} />}
 			/>
+		</ProductContext.Provider >
+			
 		</div>
 	);
 }
