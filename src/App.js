93dcf694173f78af1cf2639f5956e2e4f0053e52
Noor Al Darraji <077068@yahoo.com>
2019-08-12 16:57:52 -0500

    Step 5 done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 // Context API
-import { ProductContext } from './context/ProductContext'
+import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 
 export const ProductContext = createContext();
 function App() {
@@ -22,16 +23,18 @@ function App() {
 	return (
 		<div className="App">
 		<ProductContext.Provider value={{ products, addItem }}>
-			<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart }} > 
+				<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/" component={Products} />
-				)}
-			/>
+				{/* Routes */}
+				<Route exact path="/" component={Products} />
+					)}
+				/>
 
-			<Route
-				path="/cart" component={ShoppingCart} />}
-			/>
+				<Route
+					path="/cart" component={ShoppingCart} />}
+				/>
+			</CartContext.Provider >
 		</ProductContext.Provider >
 			
 		</div>
